ea8ac1d8afae6bf8d1e4101ead67287f761d2f96
rriazn <rnriazanov@gmail.com>
2025-06-05 13:54:18 +0200

    Started with TS

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
+  input1 : number | string = '';
+  input2 : number | string = '';
+  output : number | string = '';
 
+  calculate(operation: string) {
+    console.log(this.input1);
+  }
+  
 }
