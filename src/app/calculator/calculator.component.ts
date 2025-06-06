acdadd86e3dc7ce53c8595d2fcbfdcac37972fed
rriazn <rnriazanov@gmail.com>
2025-06-06 12:26:15 +0200

    Finished

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -8,12 +8,45 @@ import { FormsModule, NgModel } from '@angular/forms';
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
-  input1 : number | string = '';
-  input2 : number | string = '';
-  output : number | string = '';
+  input1 : number = 0;
+  input2 : number = 0;
+  out : number | string = '';
 
   calculate(operation: string) {
-    console.log(this.input1);
+    switch(operation) {
+      case '+':
+        this.out = this.input1 + this.input2;
+        break;
+      case '-':
+        this.out = this.input1 - this.input2;
+        break;
+      case '*':
+        this.out = this.input1 * this.input2;
+        break;
+      case '/':
+        this.out = this.input1 / this.input2;
+        break;
+      case 'sin':
+        this.out = Math.sin(this.input1);
+        break;
+      case 'cos':
+        this.out = Math.cos(this.input1);
+        break;
+      case '^':
+        this.out = Math.pow(this.input1, this.input2);
+        break;
+      case 'sqrt':
+        this.out = Math.sqrt(this.input1);
+        break;
+      default:
+        console.log("wrong operator");
+    }
+  }
+
+  reset() {
+    this.input1 = 0;
+    this.input2 = 0;
+    this.out = '';
   }
   
 }
